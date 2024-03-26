44cdba6fef319d57ece0ae7721e2e10baf9618d6
Tanusri Harish <118092429+tanusrixh@users.noreply.github.com>
2024-03-26 05:09:16 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion, useScroll, useTransform } from "framer-motion"
+import { useEffect, useRef, useState } from "react"
+import face from './Tanu.jpg'
+import jsonData from './info.json'
+import BoxData from './BoxData'
 
 function App() {
+
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+  const opacity = useTransform(
+    scrollYProgress,
+    [0, 0.5, 1],
+    [0, 0.5, 1],
+)
+  const opacityTwo = useTransform(
+    scrollYProgress,
+    [1, 0.25, 0],
+    [0, 0.75, 1],
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div ref={ref}>
+      <motion.div style={{y: textY, opacity: opacityTwo}} className="App">
+        <img src={face} style={{height:'45%', borderRadius:'50%', overflow:'hidden'}}></img>
+        <h1 style={{fontSize:'60px', marginBottom:'0.5em'}}>
+          Hi. My name is Tanusri Harish.
+          </h1>
+        <p 
+          style=
+            {{marginTop:'0', 
+            paddingLeft:'25%', 
+            paddingRight:'25%', 
+            textAlign:'center', 
+            fontSize:'20px'}}> 
+              I am a Computer Science and Biology Student at Western University currently working as a full-stack software developer for the Constas Research Lab at Western University. (Scroll down for more info!)
+          </p>
+          
+      </motion.div>
+      <motion.div style={{ y: backgroundY }} className='App-header'>
+        <motion.h1 style={{opacity: opacity, marginLeft:'5%'}}>
+          Projects
+        </motion.h1>
+        <motion.div  style={{opacity: opacity, overflowY:'auto', width:'70vw'}}>
+          {jsonData.projInfo.map((dataItem, index) => (
+            <BoxData title={dataItem.title} desc={dataItem.desc} link={dataItem.link}/>
+          ))}
+        </motion.div>
+
+      </motion.div>
+    </div> 
   );
 }
 
---
+++ src/BoxData.js
@@ -0,0 +1,67 @@
+import { Box, Typography } from "@mui/material"
+
+
+export default function BoxData({title, desc, link}) {
+
+    const newText = desc.slice(0, 250)
+
+
+    return (
+        <Box sx={{width: '70vw', 
+        height:'25vh', 
+        alignItems: 'center', 
+        justifyContent:'center',
+        display:'flex',
+        marginY:'1%',
+        borderRadius:'20px',
+        backgroundColor:'white'
+        }}>
+            
+                    <Box sx={{width: '30%',
+                            height:'80%',
+                            backgroundColor:'white',
+                            borderTopLeftRadius:'20px',
+                            borderBottomLeftRadius:'20px',
+                            borderRight:'2px solid grey',
+                            paddingRight:'1%',
+                            paddingLeft:'1%',
+                            alignItems:"center",
+                            justifyContent:'center',
+                            display:'flex',
+                            flexDirection:'column'
+                            }}>
+                                <h4 style={{textAlign:'center'}}>
+                                    {title}
+                                </h4>
+                        
+                    </Box>
+                    <Box sx={{width: '70%',
+                            height:'100%',
+                            backgroundColor:'white',
+                            overflowY:'auto',
+                            alignItems: 'flex-start',
+                            justifyContent:'center', 
+                            display:'flex',
+                            flexDirection:'column',
+                            borderTopRightRadius:'20px',
+                            borderBottomRightRadius:'20px'
+                            }}>
+                                <Box sx={{display:'flex', flexDirection:'column', marginLeft:'2%', marginRight:'2%'}}>
+                                    <Typography variant="body1" style={{ textAlign: 'left' }}>
+                                        {newText}
+                                    </Typography>
+                                    <a href={link} target="_blank" rel="noopener noreferrer" style={{ textAlign: 'left', fontSize:'15px' }}>
+                                        Link to GitHub Repo
+                                    </a>
+                                </Box>
+                                
+            
+                                
+                                
+                    </Box>
+
+            
+
+        </Box>
+    )
+}
